b31ad4d2fe0f70ff4c072b38bed70765ade3416d
NicolasGO2112 <nicolas.galleguillos01@alumnos.ucn.cl>
2024-10-05 18:19:02 -0300

    refactor: User model refactor

---
+++ src/Models/User.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Taller_1_IDWM.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Rut { get; set; } = string.Empty;
+        public DateOnly Birthdate { get; set; }
+        public string Mail { get; set; } = string.Empty;
+        [StringLength(255, MinimumLength = 8)]
+        public string Name { get; set; } = string.Empty;
+         [RegularExpression(@"MASCULINO|FEMENINO|PREFIERO NO DECIRLO|OTRO")]
+        public string Gender { get; set; } = string.Empty;
+        [StringLength(20, MinimumLength = 8)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
