04fdae906ae86868510473fce417c0e0f7a42e71
AlbertoLyons <alberto.lyons@alumnos.ucn.cl>
2024-10-29 19:43:03 -0300

    feat: added userRepository

---
+++ src/DTOs/UserDTO.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+namespace Taller_1_IDWM.src.DTOs
+{
+    public class UserDTO
+    {
+        public string Rut { get; set; } = string.Empty;
+        public DateTime Birthdate { get; set; }
+        public string Mail { get; set; } = string.Empty;
+        [StringLength(255, MinimumLength = 8)]
+        public string Name { get; set; } = string.Empty;
+         [RegularExpression(@"Masculino|Femenino|Prefiero no decirlo|otro")]
+        public string Gender { get; set; } = string.Empty;
+        [StringLength(20, MinimumLength = 8)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
