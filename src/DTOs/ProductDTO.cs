e693080bbc9ee4543636ddf1e123cda062a902e5
NicolasGO2112 <nicolas.galleguillos01@alumnos.ucn.cl>
2024-11-05 16:01:24 -0300

    fix: move data annotations from Models to DTOs

---
+++ src/DTOs/ProductDTO.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Taller_1_IDWM.src.DTOs
+{
+    public class ProductDTO
+    {
+        
+    }
+}
